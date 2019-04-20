bdcbfce0400050a44bfc07b82d67413d0e00c46f
Phoebe Quincy <phoebequincy@gmail.com>
2019-04-19 19:04:33 -0600

    Updated app.js with correct router paths.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/ropeinst');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/ropeinst', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
